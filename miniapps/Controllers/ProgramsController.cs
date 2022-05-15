491de38fdc53d8caa0866c116411653f610214a1
Zarthan <46505448+Zarthan66@users.noreply.github.com>
2022-05-15 09:51:42 +0700

    Add project files.

---
+++ miniapps/Controllers/ProgramsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace miniapps.Controllers
+{
+    public class ProgramsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Sorter()
+        {
+            return View();
+        }
+    }
+}
