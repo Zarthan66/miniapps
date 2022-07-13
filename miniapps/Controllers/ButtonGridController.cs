7e6c955a2d79af61b00fefb142d2106d66874dc2
Zarthan <46505448+Zarthan66@users.noreply.github.com>
2022-07-13 18:29:22 +0700

    Add Buttton Grid Mini Game

---
+++ miniapps/Controllers/ButtonGridController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using miniapps.Models;
+
+namespace miniapps.Controllers
+{
+    public class ButtonGridController : Controller
+    {
+        static List<ButtonGridModel> buttons = new List<ButtonGridModel>();
+        Random random = new Random();
+        const int GRID_SIZE = 25;
+
+        public IActionResult Index()
+        {
+            if(buttons.Count == 0)
+            {
+                for (int i = 0; i < GRID_SIZE; i++)
+                {
+                    buttons.Add(new ButtonGridModel { Id = i, State = random.Next(4) });
+                }
+            }       
+            return View("Index", buttons);
+        }
+
+        public IActionResult HandleButtonClick(string buttonNumber)
+        {
+            // convert from string into int
+            int btn = int.Parse(buttonNumber);
+
+            buttons.ElementAt(btn).State = (buttons.ElementAt(btn).State + 1) % 4;
+            return View("index", buttons);
+        }
+    }
+}
---
+++ miniapps/Models/ButtonGridModel.cs
@@ -0,0 +1,8 @@
+﻿namespace miniapps.Models
+{
+    public class ButtonGridModel
+    {
+        public int Id { get; set; }
+        public int State { get; set; }
+    }
+}
